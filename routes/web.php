c0c951e51aea4597929ef7501238c454684bb7bd
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-01-12 23:33:51 +0700

    add name

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', [BlogController::class, 'index']);
+Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 
-Route::get('/blog/{blog:slug}', [BlogController::class, 'single']);
+Route::get('/blog/{blog:slug}', [BlogController::class, 'single'])->name('blog.slug');
