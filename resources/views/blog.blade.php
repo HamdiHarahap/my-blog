c5840c23f86fa9b8a6aeefc1524f2555e760599a
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-01-12 23:08:12 +0700

    fix href

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -9,7 +9,7 @@
                         <span class="text-sm">{{$blog->created_at->diffForHumans()}}</span>
                     </div>
                     <div class="h-full">
-                        <a href="/posts/{}" class="hover:underline">
+                        <a href="/blog/{{$blog->slug}}" class="hover:underline">
                             <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                                 {{$blog->title}}   
                             </h2>
@@ -19,7 +19,7 @@
                         </p>
                     </div>
                     <div class="text-right">
-                        <a href="/posts/" class="inline-flex items-center font-medium text-blue-600 hover:underline text-sm">
+                        <a href="/blog/{{$blog->slug}}" class="inline-flex items-center font-medium text-blue-600 hover:underline text-sm">
                             Read more
                             <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
